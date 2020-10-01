0bafa09f2fe081f593d200f204ce064f08a1fada
Katie Fogarty <68407771+kmf4zd@users.noreply.github.com>
2020-10-01 18:31:12 -0400

    for loop next & step difference

---
+++ code-snippets/forloop.cpp
@@ -0,0 +1,16 @@
+//Debugger Tutorial: Step & Next with For Loops
+//October 1, 2020
+
+#include <iostream>
+using namespace std;
+
+void print() {
+  cout << "Hello, World" << endl;
+}
+
+int main() {
+  for (int i = 0; i < 3; i++) {
+    print();
+  }
+  return 0;
+}
